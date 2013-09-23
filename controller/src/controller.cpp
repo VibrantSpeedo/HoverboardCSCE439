37f9e097c309518ac7303991dfecd28810411077
VibrantSpeedo <tcsweetser@hotmail.com>
2013-09-23 00:13:52 -0500

    Updated translations calculations
    I made changes that will calculate the power going to the translational thrusters given an arbitrary angle.  The angle is calculated from the x and y coordinates of the joystick.

--- controller/src/controller.cpp
+++ controller/src/controller.cpp
@@ -9,6 +9,8 @@
 #define DEADZONE .15
 #define MAX_RATE 619
 #define MIN_RATE -363
+#define PI 3.14159
+
 
 long map(long x, long in_min, long in_max, long out_min, long out_max)
 {
@@ -70,7 +72,93 @@ void Controller::controllerCallback(const controller::HovercraftControl::ConstPt
     float y = (fabs((float)hovercraftControl->y_translation) > DEADZONE) ? hovercraftControl->y_translation : 0;
     float rotation = (fabs((float)hovercraftControl->rotation) > DEADZONE) ? hovercraftControl->rotation : 0;
     
+    float a1,b1,c1,a2,b2,c2; //linear system of equations
+    float out1,out2,out3; //solution to system of equations
+    float inMag, inAngle; //input vector angle and magnitude 
+    
     thruster.lift = hovercraftControl->lift;
+    
+    inMag = sqrtf(-x*-x+y*y);
+    inAngle = atan2f(y, -x);
+    
+    // angle will be in radians between 0 and 2*pi
+    if (inAngle<0){
+        inAngle = inAngle + 2*PI;
+    }
+    
+    // angle between 0 and 90 or 330 and 360
+    if((inAngle>=0 && inAngle<=1.5708) || (inAngle>=5.7596 && inAngle<=6.2832))
+    {
+        a1 = 1.0; //sin(90)
+        b1 = -0.5; //sin(330)
+        c1 = sin(inAngle);
+        a2 = 0; //cos(90)
+        b2 = 0.8660; //cos(330)
+        c2 = cos(inAngle);
+        
+        out1 = (c1*b2-c2*b1)/(a1*b2-a2*b1);
+        out2 = 0;
+        out3 = (a1*c2-a2*c1)/(a1*b2-a2*b1);
+    }
+    // angle between 90 and 210
+    else if(inAngle >= 1.5708 && inAngle<=3.6652)
+    {
+        a1 = 1.0; //sin(90)
+        b1 = -0.5; //sin(210)
+        c1 = sin(inAngle);
+        a2 = 0; //cos(90)
+        b2 = -0.8660; //cos(210)
+        c2 = cos(inAngle);
+        
+        out1 = (c1*b2-c2*b1)/(a1*b2-a2*b1);
+        out2 = (a1*c2-a2*c1)/(a1*b2-a2*b1);
+        out3 = 0;
+    }
+    // angle between 210 and 330
+    else if(inAngle >= 3.6652 && inAngle<=5.7596)
+    {
+        a1 = -0.5; //sin(210)
+        b1 = -0.5; //sin(330)
+        c1 = sin(inAngle);
+        a2 = -0.8660; //cos(210)
+        b2 = 0.8660; //cos(330)
+        c2 = cos(inAngle);
+        
+        out1 = 0;
+        out2 = (c1*b2-c2*b1)/(a1*b2-a2*b1);
+        out3 = (a1*c2-a2*c1)/(a1*b2-a2*b1);
+    }
+    else
+    {
+        // not moving
+        out1 = 0;
+        out2 = 0;
+        out3 = 0;
+    }
+    
+    // Finding maximum to scale down to 1.
+    float max = 0;
+    
+    if(out1>out2){
+        if(out1>out3){
+            max = out1;
+        }
+    }
+    else if(out2>out1){
+        if(out2>out3){
+            max = out2;
+        }
+    }
+    else if(out3>out2){
+        if(out3>out1){
+            max = out3;
+        }
+    }
+
+    thruster.thruster1 = inMag*(out1/max);
+    thruster.thruster2 = inMag*(out2/max);
+    thruster.thrutser3 = inMag*(out3/max);
+    /*
     if(x>0)
     {
        thruster.thruster1 = x*0.5;
@@ -106,7 +194,7 @@ void Controller::controllerCallback(const controller::HovercraftControl::ConstPt
     {
        thruster.thruster3 = 1.0;
     }
-    
+    */
     if(rotation>0)
     {
       thruster.thruster4 = rotation;
