8bc6a39cee1c025678903b580efc9cf689837179
Elliott <john.elliott.mccoy@gmail.com>
2013-09-23 09:51:50 -0500

    Added X/Y Translation ability

--- controller/src/controller.cpp
+++ controller/src/controller.cpp
@@ -5,21 +5,16 @@
 #include <controller/HovercraftControl.h>
 #include <cmath>
 
-
 #define DEADZONE .15
 #define MAX_RATE 619
 #define MIN_RATE -363
-
-long map(long x, long in_min, long in_max, long out_min, long out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+#define PI 3.14159
 
 class Controller
 {
 public:
   Controller();
-
+  
 private:
   void controllerCallback(const controller::HovercraftControl::ConstPtr& hovercraftControl);
   void gyroCallback(const hovercraft::Gyro::ConstPtr& data);
@@ -61,58 +56,74 @@ void Controller::controllerCallback(const controller::HovercraftControl::ConstPt
   thruster.thruster5 = 0; //Left
   //Unused
   thruster.thruster6 = 0; //
-
+  
   if(hovercraftControl->power == 1)
   { 
-    //left to right 1 to -1
-    //up to down 1 to -1
-    float x = (fabs((float)hovercraftControl->x_translation) > DEADZONE) ? hovercraftControl->x_translation : 0;
-    float y = (fabs((float)hovercraftControl->y_translation) > DEADZONE) ? hovercraftControl->y_translation : 0;
+    float x = -(hovercraftControl->x_translation);
+    float y = hovercraftControl->y_translation;
     float rotation = (fabs((float)hovercraftControl->rotation) > DEADZONE) ? hovercraftControl->rotation : 0;
     
     thruster.lift = hovercraftControl->lift;
-    if(x>0)
+    
+    if(fabs(x) > DEADZONE || fabs(y) > DEADZONE)
     {
-       thruster.thruster1 = x*0.5;
-       thruster.thruster2 = x;
-    }
-    else if(x<0)
-    {
-       thruster.thruster1 = -x*0.5;
-       thruster.thruster3 = -x;
-    }
+      float angle = atan2(y, x);
+      if(angle < 0)
+      {
+        angle += 2*PI;
+      }
+      
+      float a = 0;
+      float b = 0;
+      float c = 0;
 
-    if(hovercraftControl->y_translation>0)
-    {
-      thruster.thruster1 += y;
-    } 
-    else if(y<0)
-    {
-      thruster.thruster2 += fabs(y);
-      thruster.thruster3 += fabs(y);
-    }
-    
-    if(thruster.thruster1 > 1.0)
-    {
-       thruster.thruster1 = 1.0;
-    }
-    
-    if(thruster.thruster2 > 1.0)
-    {
-       thruster.thruster2 = 1.0;
-    }
-    
-    if(thruster.thruster3 > 1.0)
-    {
-       thruster.thruster3 = 1.0;
+      angle *= 180/PI;
+      /*if(angle < 95 && angle > 85)
+      {
+        a = 1;
+      }
+      else if(angle < 215 && angle > 205)
+      {
+        b = 1;
+      }
+      else if(angle < 335 && angle > 325)
+      {
+        c = 1;
+      }
+      else*/ if(angle >= 90 && angle < 210)
+      {
+        b = -2*x/sqrt(3);
+        a = y + b/2;
+      } 
+      else if (angle >= 210 && angle < 330)
+      {
+        c = (x - sqrt(3)*y)/sqrt(3);
+        b = -2*y-c;
+      }
+      else 
+      {
+        c = 2*x/sqrt(3);
+        a = y + c/2;
+      }
+      
+      if(a < 0) a = 0; if(a > 1) a = 1;
+      if(b < 0) b = 0; if(b > 1) b = 1;
+      if(c < 0) c = 0; if(c > 1) c = 1;
+
+      thruster.thruster1 = a;
+      thruster.thruster2 = b;
+      thruster.thruster3 = c;
+      thruster.angle = angle;   
     }
     
     if(rotation>0)
     {
       thruster.thruster4 = rotation;
+      thruster.thruster5 = 0;
     } 
     else if(rotation<0)
     {
+      thruster.thruster4 = 0;
       thruster.thruster5 = fabs(rotation);
     }
     
@@ -128,6 +139,6 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "controller");
   Controller controller;
-
+  
   ros::spin();
 }
